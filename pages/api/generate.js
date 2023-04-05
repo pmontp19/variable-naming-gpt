7ea8b41f83cef003128d8173f86719938697f39d
Pere Montpeo <pere.montpeo@decathlon.com>
2023-04-05 18:30:26 +0200

    init

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,62 +1,80 @@
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from 'openai'
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+  apiKey: process.env.OPENAI_API_KEY
+})
+const openai = new OpenAIApi(configuration)
 
 export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
+        message:
+          'OpenAI API key not configured, please follow instructions in README.md'
       }
-    });
-    return;
+    })
+    return
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const variable = req.body.variable || ''
+  const casing = req.body.case || ''
+  if (variable.trim().length === 0 || casing.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: 'Please submit a valid form'
       }
-    });
-    return;
+    })
+    return
   }
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
-    });
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: generatePrompt(variable, casing),
+      temperature: 0.6
+    })
+    res.status(200).json({ result: completion.data.choices[0].message.content })
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
+      console.error(error.response.status, error.response.data)
+      res.status(error.response.status).json(error.response.data)
     } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
+      console.error(`Error with OpenAI API request: ${error.message}`)
       res.status(500).json({
         error: {
-          message: 'An error occurred during your request.',
+          message: 'An error occurred during your request.'
         }
-      });
+      })
     }
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+function generatePrompt(variable, casing) {
+  const capitalizedVariable =
+    variable[0].toUpperCase() + variable.slice(1).toLowerCase()
+  return [
+    {
+      role: 'system',
+      content:
+        'You are an expert senior programmer with years of experience naming variables in different programming lenguages'
+    },
+    {
+      role: 'user',
+      content: `Suggest three names for a code variable that are hilarious. Complete only the three names, comma separeted. Use ${casing} case.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+                Variable: For loops
+                Names: i, ii, iii, iiii, iiiii
+                Varibale: Speed
+                Names: direction_change_velocity_retainment_coyote_time
+                Varibale: Color
+                Names: gray, grayPlus, grayPlusLight, grayDoublePlusLight
+                Varibale: Int
+                Names: tint
+                Varibale: String builder object
+                Names: bobTheBuilder
+                Variable: ${capitalizedVariable}
+                Names:`
+    }
+  ]
 }
--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,79 @@
-import Head from "next/head";
-import { useState } from "react";
-import styles from "./index.module.css";
+import Head from 'next/head'
+import { useState } from 'react'
+import styles from './index.module.css'
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+  const [variableInput, setVariableInput] = useState('')
+  const [caseInput, setCaseInput] = useState('')
+  const [result, setResult] = useState()
 
   async function onSubmit(event) {
-    event.preventDefault();
+    event.preventDefault()
     try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
+      const response = await fetch('/api/generate', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ animal: animalInput }),
-      });
+        body: JSON.stringify({ variable: variableInput, case: caseInput })
+      })
 
-      const data = await response.json();
+      const data = await response.json()
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        )
       }
 
-      setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
+      setResult(data.result)
+      setVariableInput('')
+    } catch (error) {
       // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
+      console.error(error)
+      alert(error.message)
     }
   }
 
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>Variable namer</title>
+        <link rel="icon" href="/code.png" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <img src="/code.png" className={styles.icon} />
+        <h3>Name my variable</h3>
+        <blockquote>
+          There are only two hard things in Computer Science: cache invalidation
+          and naming things. Phil Karlton
+        </blockquote>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="variable"
+            placeholder="The result of an API call"
+            value={variableInput}
+            onChange={(e) => setVariableInput(e.target.value)}
           />
+          <select
+            name="casing"
+            id="case-select"
+            value={caseInput}
+            onChange={(e) => setCaseInput(e.target.value)}
+          >
+            <option value="">Select casing</option>
+            <option value="pascal">Pascal case</option>
+            <option value="camel">Camel case</option>
+            <option value="snake">Snake case</option>
+            <option value="kebab">Kebab case</option>
+          </select>
+
           <input type="submit" value="Generate names" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
     </div>
-  );
+  )
 }
